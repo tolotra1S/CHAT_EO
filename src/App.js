fc7cd72a4b24fa87527c7d2a1aa48939f96dd64f
tolotra1S <tolotraranarison@gmail.com>
2023-04-06 22:41:07 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Auth from './components/Auth';
+import { useState } from 'react';
 
 function App() {
+  const [isAuth,SetIsAuth]= useState(cookies.get("auth-token"));
+  const [room, SetRoom] = useState(null);
+
+  if(!isAuth){
   return (
     <div className="App">
       Wait. .... ..
       <Auth/>
     </div>
   );
+  }
+    return (
+      <div className="App">
+        {room ?( <div>Chat : </div> ):( <div>
+        <label>Enter Room Name:</label>
+        <input/>
+        </div>)}
+      </div>
+    );
+    
+  
 }
 
 export default App;
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {auth,provider} from "../firebaseConfig.js";
 import {signInWithPopup} from "firebase/auth";
-import Cookies from 'universal-cookie'
+import Cookies from 'universal-cookie';
+const cookies = new Cookies;
 
 const Auth = () => {
     const signInWithGoogle = async () =>{
+        try {
         const result = await signInWithPopup(auth,provider);
         console.log(result);
+        cookies.set("auth-token",result.user.refreshToken);
+        }
+        catch(err){
+            console.error(err)
+
+        }
     }
     return (
         <div>
