a6dbc75857e6dc754f10e13a0e1c1a8c97df009e
tolotra1S <tolotraranarison@gmail.com>
2023-04-09 12:32:43 +0300

    final commit

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,29 +1,65 @@
-import React from 'react';
-import {auth,provider} from "../firebaseConfig.js";
-import {signInWithPopup} from "firebase/auth";
+import React, { useState, useEffect } from 'react';
+import { auth, provider } from "../firebaseConfig.js";
+import { signInWithPopup } from "firebase/auth";
 import Cookies from 'universal-cookie';
+
 const cookies = new Cookies();
 
-const Auth = (props) => {
-    const{setIsAuth} = props;
-    const signInWithGoogle = async () =>{
-        try {
-        const result = await signInWithPopup(auth,provider);
-        console.log(result);
-        cookies.set("auth-token",result.user.refreshToken);
-        setIsAuth(true)
-        }
-        catch(err){
-            console.error(err)
+const Auth = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState(null);
 
-        }
+  useEffect(() => {
+    const authToken = cookies.get('auth-token');
+    if (authToken) {
+      auth.signInWithCustomToken(authToken)
+        .then((result) => {
+          setUser(result.user);
+          setIsAuth(true);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
-    return (
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user);
+      setIsAuth(true);
+      cookies.set('auth-token', await result.user.getIdToken(), { path: '/', maxAge: 86400 * 7 });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      setUser(null);
+      setIsAuth(false);
+      cookies.remove('auth-token', { path: '/' });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      {isAuth ? (
         <div>
-            <p>Sign in with google To continue</p>
-            <button onClick={signInWithGoogle}>Sign in With Google</button>
+          <p>Welcome {user.displayName}</p>
+          <button onClick={handleSignOut}>Sign Out</button>
         </div>
-    );
-}
+      ) : (
+        <div>
+          <p>Sign in with Google to continue</p>
+          <button onClick={signInWithGoogle}>Sign in With Google</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Auth;
